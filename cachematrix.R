242b2c4857857f602be20ec6bcd01c2f7096f488
DylanStudent2 <ykj195@my.cs.utsa.edu>
2015-05-24 10:50:14 -0500

    The program

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+#!/usr/bin/env Rscript
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#Time to make a function dedicated to saving a result
+makeCacheMatrix <- function(value = matrix(1:9, nrow = 3, ncol = 3)){
+   cache <- NULL
+
+   #Have a new matrix replace the previous one, where 
+   #We don't know the inverse of, so we clear the cache.
+   set <- function(newValue){
+      value <<- newValue 
+      cache <<- NULL
+   }
+
+   get <- function() value
+
+   #After a solve function is used, the result should
+   #be passed through this function to update the cache.
+   setInverse <- function(solveResult){
+         cache <<- solveResult 
+   }
+
+   getInverse <- function() cache
+
+   list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
+}
+
+
+cacheSolve <- function(data, ...){
+   result <- data$getInverse()
+   
+   if(!is.null(result) ){
+      print("Same Inverse returned")
+      return(result)
+   }
+
+   value <- data$get()
+   result <- solve(value, ...) 
+   data$setInverse(result)
+   result
 
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
